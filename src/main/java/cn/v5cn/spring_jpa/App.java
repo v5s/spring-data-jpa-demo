535bfe2227ed012187a48175197541ddf753aeb6
ZYW <zyw090111@163.com>
2014-07-15 11:33:44 +0800

    init spring data jpa

---
+++ src/main/java/cn/v5cn/spring_jpa/App.java
@@ -0,0 +1,13 @@
+package cn.v5cn.spring_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
